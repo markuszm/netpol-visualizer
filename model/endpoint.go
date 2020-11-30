645831fb996996b79fc87fe4f72b8d6cbe3e080f
Simon Heuser <simon.heuser@qaware.de>
2020-11-30 17:26:55 +0100

    add netpol type to model

--- model/endpoint.go
+++ model/endpoint.go
@@ -1,15 +1,18 @@
 package model
 
+import v1 "k8s.io/api/networking/v1"
+
 type Pod struct {
 	// Key string
 	Name      string
 	Namespace string
 }
 
-type UnrestrictedEdge struct {
+type PolicyEdge struct {
+	PolicyType v1.PolicyType
 	From, To Pod
 	// if not specified, allow all ports
 	Port int
 }
 
-type Policies = []UnrestrictedEdge
+type Policies = []PolicyEdge
